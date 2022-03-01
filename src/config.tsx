bc29f5508ac16f8407311ff19b8f2768d601d98e
yum0e <yum0e@protonmail.com>
2022-03-01 13:57:42 +0100

    added mainnet top20 deadrare collections

--- src/config.tsx
+++ src/config.tsx
@@ -3,4 +3,25 @@ export const contractAddress =
 
 export const dAppName = 'middleman.nft';
 
-export const verified = ['TST-9224fc'];
+export const verified = [
+  'AERMES-ac9886',
+  'BDGA-a05446',
+  'DRIFTERS-efd96c',
+  'EAPES-8f3c1f',
+  'EGLDHUNTER-9abc5c',
+  'GNOGEN-8156fb',
+  'GNOGONS-73222b',
+  'GUARDIAN-3d6635',
+  'LIGHTNINGB-496265',
+  'MAIARPUNKS-3db7e8',
+  'MANY-39af2c',
+  'PITTZ-1a4c2d',
+  'PRE-807532',
+  'QUACK-f01e02',
+  'RAPTOR-28e21c',
+  'SRC-27d8ff',
+  'SYMB-a060b0',
+  'VALIDATORS-e7e287',
+  'WARRIORZ-2f0986',
+  'WWWINE-5a5331'
+];
